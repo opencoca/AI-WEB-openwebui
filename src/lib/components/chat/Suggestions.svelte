71a8042cc0d54af794df037b9f8d4ef4bb8c7452
OpenCo <admin@openco.ca>
2024-10-19 17:40:18 +0000

    refactor: Update greeting message and translation in ChatPlaceholder and Suggestions components

--- src/lib/components/chat/Suggestions.svelte
+++ src/lib/components/chat/Suggestions.svelte
@@ -19,29 +19,30 @@
 	<div id="suggestions-header" 
 	class="mb-1 flex gap-1 text-sm font-medium items-center text-gray-400 dark:text-gray-600">
 		<Bolt />
-		{$i18n.t('Suggestions')}
+		{$i18n.t('Search')} {$i18n.t('Suggestions')}
 	</div>
 {/if}
 
-<div class="{className}" style="--h: 0; --minh: 200%; --of: scroll;">
+<div class="{className}" style="--minh: 200%; --of: scroll;">
 	{#each prompts as prompt, promptIdx}
-		<div class="prompt" style=" --shadow: 6; --levitate-hvr:8; --br: 1rem; --p: 1em; --m:0.6em; --d: flex;"
+		<div class="prompt" style=" --shadow: 6; 
+									--levitate-hvr:8; 
+									--br: 1rem; 
+									--p: 1em; 
+									--m:0.6em; 
+									--d: flex;"
 			on:click={() => {
 				dispatch('select', prompt.content);
 			}}
 		>
 			<div class="flex flex-col text-left">
 				{#if prompt.title && prompt.title[0] !== ''}
-					<div
-						style="--line-clamp:4;"
-					>
+					<h5>
 						{prompt.title[0]}
-					</div>
-					<div style="--line-clamp:4;">{prompt.title[1]}</div>
+					</h5>
+					<div style="--size:0.8em">{prompt.title[1]}</div>
 				{:else}
-					<div
-						style="--line-clamp:4;"
-					>
+					<div>
 						{prompt.content}
 					</div>
 
